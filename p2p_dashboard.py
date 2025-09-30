f01aa8eed0df726ab9ebf66139cc2b664d40d186
Pauriktrivedi <96005961+Pauriktrivedi@users.noreply.github.com>
2025-09-30 12:54:26 +0530

    Update p2p_dashboard.py

--- p2p_dashboard.py
+++ p2p_dashboard.py
@@ -4,6 +4,9 @@ import pandas as pd
 import streamlit as st
 import plotly.express as px
 import plotly.graph_objects as go
+import io
+
+
 
 # ====================================
 #  Procure-to-Pay Dashboard (Streamlit)
